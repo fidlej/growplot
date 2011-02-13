fce56616ef9ce9d25d645336d22e4f1dc485f4a4
Ivo Danihelka <ivo@danihelka.net>
2011-02-13 22:42:47 +0100

    Allowed to read the values from a file.

--- growplot.py
+++ growplot.py
@@ -1,8 +1,48 @@
 #!/usr/bin/env python
 
+import os
+
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib import pyplot
+
+
+class TailReader:
+    def __init__(self, filename):
+        self.fd = os.open(filename, os.O_RDONLY|os.O_NONBLOCK)
+        self.remainder = ""
+
+    def nextvalues(self):
+        """Returns next found values from the file.
+        """
+        data = self._read_available(self.fd)
+        if not data:
+            return []
+
+        lines = (self.remainder + data).split("\n")
+        self.remainder = lines[-1]
+        return [float(line) for line in lines[:-1]]
+
+    def _read_available(self, fd):
+        """Reads all available bytes
+        from the non-blocking fd.
+        """
+        buf = ""
+        while True:
+            try:
+                bytes = os.read(fd, 8192)
+                if len(bytes) == 0:
+                    return buf
+
+                buf += bytes
+            except OSError, e:
+                if e.errno == errno.EINTR:
+                    continue
+                elif e.errno == errno.EAGAIN:
+                    return buf
+                else:
+                    raise
+
 
 class Animator:
     def __init__(self, figure):
@@ -33,6 +73,14 @@ class Animator:
 
 
 def main():
+    reader = TailReader("data.log")
+    import time
+    while True:
+        values = reader.nextvalues()
+        print "values:", values
+        time.sleep(1)
+
+    return
     figure = pyplot.figure()
     animator = Animator(figure)
 
@@ -40,5 +88,5 @@ def main():
     pyplot.show()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
